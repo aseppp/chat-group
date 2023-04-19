c9ab6f68c6696d7fa0428bbd985a2591adea9ccd
aseppp <saepudinasep86@gmail.com>
2023-04-19 07:16:32 +0700

    checkpoint remove modules hapi, initialize socket io

--- server/index.js
+++ server/index.js
@@ -1,14 +1,26 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+
+const app = express();
 app.use(cors());
 app.use(express.json());
+
+const io = require('socket.io')(4000, {
+  cors: {
+    origin: ['http://localhost:3000'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(socket.id);
+});
 
 app.get('/', (req, res) => {
   res.json('Server working propperly');
 });
 
 const routes = require('./routes/routes');
+const { log } = require('console');
 app.use('/api', routes);
 
 const PORT = process.env.PORT || 5000;
